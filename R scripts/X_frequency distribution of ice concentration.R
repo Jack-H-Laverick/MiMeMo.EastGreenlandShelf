8378751adc29b29891513dd2d87977aaa8ee9de3
Jack H Laverick from Strath <jacklaverick@ymail.com>
2020-05-18 16:27:02 +0100

    Modified ice thresholds so they're plotted as probability distributions.

--- R scripts/X_frequency distribution of ice concentration.R
+++ R scripts/X_frequency distribution of ice concentration.R
@@ -12,10 +12,6 @@ SP <- readRDS("./Objects/SPATIAL.rds") %>%                                  # Re
   select(Ice_conc, Month, Shore) %>% 
   drop_na()
 
-ggplot(SP) + 
-  geom_density(aes(x = Ice_conc)) +
-  facet_wrap(vars(Month), scales = "free_y")
-
 # ggplot(SP) + 
 #   geom_density(aes(x = Ice_conc), fill = "steelblue2") +
 #   theme_minimal() +
@@ -26,11 +22,21 @@ ggplot(SP) +
 
 names(month.abb) <- seq(1:12) # Give months numeric name to work in faect
 
+ggplot(SP) + 
+  geom_density(aes(x = Ice_conc)) +
+  theme_minimal() +
+  labs(y = "Density", x = "Sea Ice Concentration", 
+       caption = "Mean monthly values at a pixel from 2000-2009") +
+  facet_wrap(vars(Month), scales = "free_y",
+             labeller = labeller(Month = month.abb[])) +
+  coord_cartesian(xlim = c(0,1), expand = F) +
+  theme(axis.text.x = element_text(angle = 90))
+
 ggplot(SP, aes(Ice_conc)) + stat_ecdf(geom = "line") +
   theme_minimal() +
-  labs(y = "Cumulative density", x = "Sea Ice Concentration", 
+  labs(y = "Cumulative density", x = "Sea Ice Concentration",
        caption = "Mean monthly values at a pixel from 2000-2009") +
-  facet_wrap(vars(Month), 
+  facet_wrap(vars(Month),
              labeller = labeller(Month = month.abb[])) +
   coord_cartesian(xlim = c(0,1), expand = F) +
   theme(axis.text.x = element_text(angle = 90))
@@ -49,6 +55,7 @@ year <- map_dfr(files, ~{
 names(month.abb) <- seq(1:12) # Give months numeric name to work in faect
 
 ggplot(filter(year, Shore == "Inshore"), aes(Ice_conc)) + stat_ecdf(geom = "line") +
+#ggplot(filter(year, Shore == "Inshore")) + geom_density(aes(x =Ice_conc)) +
   theme_minimal() +
   labs(y = "Cumulative density", x = "Sea Ice Concentration", 
        title = "Inshore zone",
@@ -60,8 +67,9 @@ ggplot(filter(year, Shore == "Inshore"), aes(Ice_conc)) + stat_ecdf(geom = "line
 
 #ggsave("Ice thesholds in.png", width = 18, height = 10, units = "cm", dpi = 500)
 
-ggplot(filter(year, Shore == "Offshore"), aes(Ice_conc)) + stat_ecdf(geom = "line") +
-  theme_minimal() +
+#ggplot(filter(year, Shore == "Offshore"), aes(Ice_conc)) + stat_ecdf(geom = "line") +
+#ggplot(filter(year, Shore == "Inshore")) + geom_density(aes(x =Ice_conc)) +
+theme_minimal() +
   labs(y = "Cumulative density", x = "Sea Ice Concentration", 
        title = "Offshore zone",
        caption = "Mean monthly values at a pixel in 2000") +
