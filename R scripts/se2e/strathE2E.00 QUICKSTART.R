8d4ad071d1e6ac3df110ed33eea67bcf0415cde9
Jack H Laverick from Strath <jacklaverick@ymail.com>
2022-03-07 16:41:51 +0000

    feat: update to use Mike's fettled files.

--- R scripts/se2e/strathE2E.00 QUICKSTART.R
+++ R scripts/se2e/strathE2E.00 QUICKSTART.R
@@ -1,8 +1,9 @@
 
 library(StrathE2EPolar)
 
-model <- e2ep_read("Greenland_Sea","2011-2019", models.path = "StrathE2E/Models/", results.path = "StrathE2E/Results/")
-
+model <- e2ep_read("Greenland_Mike","2011-2019", models.path = "StrathE2E/Models/", results.path = "StrathE2E/Results/")
+ 
 results <- e2ep_run(model,nyears = 50)
 
-plot_full_length_timeseries(model, results)
+e2ep_plot_ts(model, results)
+
